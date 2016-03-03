edc97c7d087b389c1870b980195af563cdf1f248
Gesa <vincent.rsbs@gmail.com>
2016-03-03 01:10:25 +0100

    Add setting of notifications

--- src/AppBundle/Service/NotificationService.php
+++ src/AppBundle/Service/NotificationService.php
@@ -7,6 +7,9 @@ class NotificationService {
     public function __construct(Redis $redis)
     {
         $this->redis = $redis;
+        $this->notificationsPanel = array(
+            "new-user", "add-game", "earn-points", "earn-game", "earn-badge", "subscribe-challenge"
+        );
     }
 
     public function getNotifications($userId)
@@ -14,5 +17,15 @@ class NotificationService {
         $notifications = $this->redis->keys($userId."_notif-*");
 
         return $notifications;
+    }
+
+    public function setNotification($userId, $key, $value)
+    {
+        if(!in_array($key, $this->notificationsPanel)) {
+            return;
+        }
+
+        $notification = $userId . "_" . $key;
+        $this->redis->set($notification, $value);
     }
 }
