23475fd68ff8ea1705c9bb2e8e9f58ba7f43c472
Gesa <vincent.rsbs@gmail.com>
2016-03-03 17:31:13 +0100

    DD

--- app/config/parameters.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $dirKernel = $container->getParameter('kernel.root_dir');
-    $dirUploads = $dirKernel.'/../web/uploads/';
-
-    $container->setParameter('upload.dir', $dirUploads);
-    $container->setParameter('notification.status', false);
